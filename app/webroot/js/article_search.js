6da990f981fcafcb296a3406bdacd21d20076a9e
Marko Vujovic <marko.vujo@gmail.com>
2013-01-04 22:03:27 -0500

    Full url for the ajax path

--- app/webroot/js/article_search.js
+++ app/webroot/js/article_search.js
@@ -10,7 +10,7 @@ $(function() {
 		
 		$.ajax({
             type: 'POST',
-            url: document.URL . '/articles/search',
+            url: document.URL + '/articles/search',
             data: formData,
             dataType: 'json',
             success: function(data) {
