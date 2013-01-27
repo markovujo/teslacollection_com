8762b5eb07d57718d00adeab17ee7ffc96c30211
Marko Vujovic <marko.vujo@gmail.com>
2013-01-26 20:50:42 -0500

    Authors and Publications have status and deleted timestamp

--- ddl/changes.sql
+++ ddl/changes.sql
@@ -1,2 +1,10 @@
 UPDATE articles SET status='active';
 ALTER TABLE articles ADD COLUMN deleted datetime DEFAULT NULL;
+
+ALTER TABLE authors ADD COLUMN status varchar(25) AFTER name;
+UPDATE authors SET status='active';
+ALTER TABLE authors ADD COLUMN deleted datetime;
+
+ALTER TABLE publications ADD COLUMN status varchar(25) AFTER name;
+UPDATE publications SET status='active';
+ALTER TABLE publications ADD COLUMN deleted datetime;
