ba5257bf27d683cdb194dc7ada8eed2525166f3a
Marko Vujovic <marko.vujo@gmail.com>
2013-02-10 18:07:11 -0500

    initDB readded

--- app/Plugin/Admin/Controller/UsersController.php
+++ app/Plugin/Admin/Controller/UsersController.php
@@ -26,4 +26,37 @@ class UsersController extends AdminAppController {
 	    
 	    //$this->Auth->allow('*');
 	}
+	
+	function initDB() {
+        $group = & $this->User->Group;
+        //Allow Striker to everything
+        $group->id = 1;
+        $this->Acl->allow($group, 'controllers');
+ 
+        /* Allow midfielder add/edit/view scores and edit/view users.
+         * but they can't add users or group
+         */
+ 
+        $group->id = 2;
+        $this->Acl->deny($group, 'controllers');
+        $this->Acl->allow($group, 'controllers/Scores/index');
+        $this->Acl->allow($group, 'controllers/Users/index');
+        $this->Acl->allow($group, 'controllers/Scores/add');
+        $this->Acl->allow($group, 'controllers/Scores/edit');
+        $this->Acl->allow($group, 'controllers/Scores/view');
+        $this->Acl->allow($group, 'controllers/Users/edit');
+        $this->Acl->allow($group, 'controllers/Users/view');
+        $this->Acl->allow($group, 'controllers/Users/logout');
+ 
+        /*
+         * Allow defender to view score and view users only
+         */
+        $group->id = 3;
+        $this->Acl->deny($group, 'controllers');
+        $this->Acl->allow($group, 'controllers/Scores/index');
+        $this->Acl->allow($group, 'controllers/Users/index');
+        $this->Acl->allow($group, 'controllers/Scores/view');
+        $this->Acl->allow($group, 'controllers/Users/view');
+        $this->Acl->allow($group, 'controllers/Users/logout');
+    }
 }
