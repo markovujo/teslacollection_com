ed4ad904bc68890fa8df913db0301d58e0480b60
Marko Vujovic <marko.vujo@gmail.com>
2015-06-21 21:15:38 -0400

    Code sniffer Console folder

--- app/Console/Command/SlugShell.php
+++ app/Console/Command/SlugShell.php
@@ -5,29 +5,26 @@
  * 
  * cake Import
  */
-class SlugShell extends AppShell 
-{
-    public $uses = array(
-    	'Article'	
-    	, 'Author'
-    	, 'Publication'
-    	, 'Subject'
-    );
-	
-	public function main() 
-    {
-        $this->out('SAVING ARTICLES!!');
-        
-        $articles = $this->Article->find('all', array(
-        	'conditions' => array(
-        		//'Article.id' => 1649
-        	),
-        	'contain' => false
-        ));
-        
-        $i = 1;
-        $total_count = count($articles);
-        
+class SlugShell extends AppShell {
+
+	public $uses = array(
+		'Article',
+		'Author',
+		'Publication',
+		'Subject'
+	);
+
+	public function main() {
+		$this->out('SAVING ARTICLES!!');
+
+		$articles = $this->Article->find('all', array(
+			'conditions' => array(),
+			'contain' => false
+		));
+
+		$i = 1;
+		$total_count = count($articles);
+
         //die(debug($articles));
         if($articles) {
         	foreach($articles AS $article) {
@@ -73,4 +70,3 @@ class SlugShell extends AppShell
         }
     }
 }
-?>
